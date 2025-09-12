a39e0c2f575d560f8bbe79403e537cdcadfddece
Bunnarin Chrun <chrunbunnarin857@gmail.com>
2025-09-12 12:22:35 +0700

    new checkpoint

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { goto } from '$app/navigation';
+	import { pbUser, logout } from '$lib/pocketbase';
+</script>
+
+<main class="form-container">
+	<div class="header">
+		<a href="/"><img class="logo" src="/favicon.png" alt="Logo"/></a>
+		{#if $pbUser}
+			<div class="user">
+				<button class="btn-primary" on:click={() => goto('/payway')}>Buy</button>
+				<button class="btn-secondary" on:click={() => logout()}>Logout</button>
+			</div>
+		{:else}
+		<button class="btn-primary" on:click={() => goto('/login')}>Login</button>
+		{/if}
+	</div>
+	<slot />
+</main>
