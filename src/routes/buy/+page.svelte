0657efa4bbb93d13d75570a1a7d32975c30b85f2
Bunnarin Chrun <chrunbunnarin857@gmail.com>
2025-09-19 07:20:35 +0700

    nw MVP for actsmart

--- src/routes/buy/+page.svelte
+++ src/routes/buy/+page.svelte
@@ -3,38 +3,42 @@
     import { PUBLIC_PAYWAY_ENDPOINT, PUBLIC_UNIT_PRICE } from '$env/static/public';
     export let data;
     $: amount = 1;
+    let paying = false;
 
-    function populateForm() {
-        pb.send(`/buy/${amount}`, {method: 'POST'})
-            .then(data => {
-                const form = document.getElementById('payway_form');
-                Object.entries(data).forEach(([key, value]) => {
-                    const input = document.createElement('input');
-                    input.type = 'hidden';
-                    input.name = key;
-                    if (key == "hash") {
-                        // doing this because pocketbase cannot encode base64
-                        const byteArray = new Uint8Array(value.match(/[\da-f]{2}/gi).map(h => parseInt(h, 16)));
-                        input.value = btoa(String.fromCharCode(...byteArray));
-                    }
-                    else 
-                        input.value = value;
-                    form?.appendChild(input);
-                });
-                form?.submit();
-            })
+    async function populateForm() {
+        paying = true;
+        const data = await pb.send(`/buy/${amount}`, {method: 'POST'})
+        const form = document.getElementById('payway_form');
+        Object.entries(data).forEach(([key, value]) => {
+            const input = document.createElement('input');
+            input.type = 'hidden';
+            input.name = key;
+            if (key == "hash") {
+                // doing this because pocketbase cannot encode base64
+                const byteArray = new Uint8Array(value.match(/[\da-f]{2}/gi).map(h => parseInt(h, 16)));
+                input.value = btoa(String.fromCharCode(...byteArray));
+            }
+            else 
+                input.value = value;
+            form?.appendChild(input);
+        });
+        form?.submit();
     }
 </script>
+{#if paying}
+    <p>Processing payment...</p>
+{:else}
 <form on:submit={populateForm}>
 <div class="form-question">
     <h1>Your current employees: {data.total_employees}/{data.max_employees}</h1>
     <br>
-    Amount: (Total: ${amount * PUBLIC_UNIT_PRICE})
+    Number of employees to buy: 
+    <br>
+    (Total: ${amount * PUBLIC_UNIT_PRICE})
     <input type="number" bind:value={amount} min="1" required />
     <br><br>
     <button type="submit" class="btn-primary">Buy</button>
 </div>
-    
 </form>
-
+{/if}
 <form id="payway_form" action="{PUBLIC_PAYWAY_ENDPOINT}" method="POST" target="_blank"></form>
